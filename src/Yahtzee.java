35e0ba0da27ec47d1af07e26da74851947901680
Alicia <aliciawalker43@gmail.com>
2020-08-11 22:06:50 -0400

    6 test methods

--- src/Yahtzee.java
+++ src/Yahtzee.java
@@ -1,15 +1,90 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Yahtzee {
 
-	
-	
-	
-	
-	
-	public static int sumOfDie(int dice1,int dice2,int dice3,int dice4,int dice5) {
-		 
-		int sum = dice1 +dice2 + dice3 +dice4 + dice5; 
-		
+	public static int sumOfDie(int dice1, int dice2, int dice3, int dice4, int dice5) {
+
+		int sum = dice1 + dice2 + dice3 + dice4 + dice5;
+
 		return sum;
+	}
+
+	public static int yatzy(int dice1, int dice2, int dice3, int dice4, int dice5) {
+
+		dice1 = dice2 = dice3 = dice4 = dice5;
+
+		return 50;// points
+	}
+
+	// sum of highest two die
+	public static int pair(int dice1, int dice2, int dice3, int dice4, int dice5) {
+		int sum = 0;
+		int[] die = { dice1, dice2, dice3, dice4, dice5 };
+		int highest = 0;
+		int secondHighest = 0;
+		for (int i = 0; i < die.length; i++)
+			if (die[i] > highest) {
+				secondHighest = highest;
+				highest = die[i];
+			} else if (die[i] > secondHighest) {
+				secondHighest = die[i];
+			}
+
+		sum = highest + secondHighest;
+
+		return sum;
+
+	}
+	// Four of a kind
+
+	public static boolean fourOfKind(int dice1,int dice2,int dice3,int dice4,int dice5) {
+        int[] die = {dice1,dice2,dice3,dice4,dice5};
+       int count =0;
+        for (int i = 0; i < die.length; i++){
+                  if (die.equals(die[i])){
+                      count ++;
+                  }
+        }
+        if (count == 4);
+       
+        return true;
+
 }
-}
+
+	// Three of a kind
+
+		public static boolean threeOfKind(int dice1,int dice2,int dice3,int dice4,int dice5) {
+	        int[] die = {dice1,dice2,dice3,dice4,dice5};
+	       int count =0;
+	        for (int i = 0; i < die.length; i++){
+	                  if (die.equals(die[i])){
+	                      count ++;
+	                  }
+	        }
+	        if (count == 3);
+	       
+	        return true;
+		}
+
+
+		// Small Straight 1,2,3,4,5
+
+		public static boolean smallStraight(int dice1,int dice2,int dice3,int dice4,int dice5) {
+			boolean isTrue = true; 
+			int[] die= {dice1,dice2, dice3,dice4,dice5};
+			for(int i =0; i <die.length-1; i++){ 
+			    int one = die[i];  
+			    int two= die[i+1];
+
+			     if (! (one == two-1) || (one==5 && two==1)) {
+			       isTrue=false;
+			     }
+
+			}
+			return isTrue;
+			}
+		
+	}
+
+//
--- src/YahtzeeTest.java
+++ src/YahtzeeTest.java
@@ -20,7 +20,7 @@ public class YahtzeeTest {
 	   
 	   int actual= Yahtzee.sumOfDie(4,3,5,7,6);
 	   
-	 assertEquals(20, actual);
+	 assertEquals(25, actual);
  }
  
  @Test
@@ -32,8 +32,65 @@ public class YahtzeeTest {
 	   int dice4;
 	   int dice5;
 	   
-	   int actual= Yahtzee.sumOfDie(4,3,5,7,6);
+	   int actual= Yahtzee.yatzy(4,4,4,4,4);
 	   
-	 assertEquals(20, actual);
+	 assertEquals(50, actual);
+}
+ 
+ @Test
+ void test3(){
+	 
+	   int dice1;
+	   int dice2;
+	   int dice3;
+	   int dice4;
+	   int dice5;
+	   
+	   int actual= Yahtzee.pair(4,5,6,7,1);
+	   
+	 assertEquals(13, actual);
+}
+ 
+ @Test
+ void test4(){
+	 
+	   int dice1;
+	   int dice2;
+	   int dice3;
+	   int dice4;
+	   int dice5;
+	   
+	   boolean actual= Yahtzee.fourOfKind(4,4,4,7,4);
+	   
+	 assertEquals(true, actual);
+}
+ 
+ @Test
+ void test5(){
+	 
+	   int dice1;
+	   int dice2;
+	   int dice3;
+	   int dice4;
+	   int dice5;
+	   
+	   boolean actual= Yahtzee.threeOfKind(4,3,4,5,4);
+	   
+	 assertEquals(true, actual);
+}
+
+ 
+ @Test
+ void test6(){
+	 
+	   int dice1;
+	   int dice2;
+	   int dice3;
+	   int dice4;
+	   int dice5;
+	   
+	   boolean actual= Yahtzee.smallStraight(1,5,3,4,2);
+	   
+	 assertEquals(true, actual);
 }
 }
